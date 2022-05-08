77daedcbd015ad01f058a3e308fcfba01001abe3
phaydara <phaydara@gmail.com>
2022-05-07 20:22:40 -0500

    added client side GET and saved data in redux

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
 
+  const dispatch = useDispatch();
+
   const fetchRandom = () => {
     console.log('in fetchRandom');
-    
+
+    axios.get('/random')
+      .then(response => {
+        console.log(response.data.data.images.downsized_medium.url);
+        
+        const action = {type: 'SET_RANDOM', payload: response.data.data.images.downsized_medium.url};
+        dispatch(action);
+      })
   };
 
   useEffect(() => {

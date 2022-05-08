dea55755b314441bcd604603268b6d83620e0703
phaydara <phaydara@gmail.com>
2022-05-08 13:57:30 -0500

    added limitParam to api call

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -5,8 +5,12 @@ const { default: axios } = require('axios');
 router.post('/', (req, res) => {
     const searchParam = req.body.search;
     console.log('searchParam: ', searchParam);
+
+    const limitParam = req.body.limit;
+    console.log('limitParam: ', limitParam);
     
-    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=fO8fbc5HkewBdAG3j9oSfIdDDw51gBrc&q=${searchParam}&limit=5&offset=0&rating=pg-13&lang=en`)
+    
+    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=fO8fbc5HkewBdAG3j9oSfIdDDw51gBrc&q=${searchParam}&limit=${limitParam}&offset=0&rating=pg-13&lang=en`)
         .then(response => {
             console.log(response.data.data);
             res.send(response.data.data);

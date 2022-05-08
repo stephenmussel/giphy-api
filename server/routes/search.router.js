926f8b6cbc05bbfddeb6e9eae60766b385c8a67b
phaydara <phaydara@gmail.com>
2022-05-08 13:33:46 -0500

    updated searchParam for giphy api call

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const { default: axios } = require('axios');
 
 router.post('/', (req, res) => {
-    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=fO8fbc5HkewBdAG3j9oSfIdDDw51gBrc&q=puppies&limit=5&offset=0&rating=pg-13&lang=en`)
+    const searchParam = req.body.search;
+    console.log('searchParam: ', searchParam);
+    
+    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=fO8fbc5HkewBdAG3j9oSfIdDDw51gBrc&q=${searchParam}&limit=5&offset=0&rating=pg-13&lang=en`)
         .then(response => {
             console.log(response.data.data);
             res.send(response.data.data);

514322e592cf578acfc49aa60ad752e7cc5ec3c8
phaydara <phaydara@gmail.com>
2022-05-08 19:51:07 -0500

    added footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import Search from '../Search/Search';
 import Random from '../Random/Random';
 import Header from '../Header/Header';
 import Home from '../Home/Home';
+import Footer from '../Footer/Footer';
 import { HashRouter as Router, Route } from 'react-router-dom';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route exact path="/random" component={Random} />
         <Route exact path="/search" component={Search} />
       </Router>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+    return(
+        <footer>
+            &copy; 2022
+        </footer>
+    )
+}
+
+export default Footer;
